30e107c3a54ab4f0dc3a017827f73536ce5c1baa
carmen-404 <atramyl@gmail.com>
2026-08-24 11:13:51 +0200

    Fix draws condition in global_colour_win_comparison.sql

--- db/queries/insights/global_colour_win_comparison.sql
+++ db/queries/insights/global_colour_win_comparison.sql
@@ -1,5 +1,5 @@
 SELECT
     SUM(CASE WHEN result =  'WHITE_WIN' THEN 1 ELSE 0 END) AS white_wins,
     SUM(CASE WHEN result =  'BLACK_WIN' THEN 1 ELSE 0 END) AS black_wins,
-    SUM(CASE WHEN result =  'WHITE_WIN' THEN 1 ELSE 0 END) AS draws
+    SUM(CASE WHEN result =  'DRAW' THEN 1 ELSE 0 END) AS draws
 FROM MATCHES;
